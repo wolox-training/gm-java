85d83ddce5efd001b8f972720ae0f4f747245c62
Gmarn <marn.greg@gmail.com>
2020-03-12 16:00:52 -0300

    Kickoff

---
+++ src/main/java/wolox/gmjavatraining/GmJavaTrainingApplication.java
@@ -0,0 +1,13 @@
+package wolox.gmjavatraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmJavaTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmJavaTrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wolox/gmjavatraining/GmJavaTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package wolox.gmjavatraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmJavaTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
